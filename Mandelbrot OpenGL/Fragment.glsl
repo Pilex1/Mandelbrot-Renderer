f8af70d6d368c14312bc941f1227232e71d66ae5
Alex Tan <torc.madra@hotmail.com>
2016-09-26 23:04:30 +1000

    Interactivity

--- Mandelbrot OpenGL/Fragment.glsl
+++ Mandelbrot OpenGL/Fragment.glsl
@@ -8,29 +8,32 @@ uniform float aspectRatio;
 uniform vec2 pos;
 uniform float zoom;
 
-const int maxIter = 1500;
-const int specLen = 30;
+uniform mat4 rot;
+
+uniform int maxIter;
+const int specLen = 50;
 
 const float ln2 = log(2);
 
-vec3 colours[5] = vec3[] (
-	vec3(163, 233, 237), 
-	vec3(215, 67, 18), 
-	vec3(0, 0, 0), 
-	vec3(78, 34, 210), 
-	vec3(13, 103, 228)
+vec3 colours[6] = vec3[] (
+vec3(64, 63, 42),
+vec3(215, 123, 25),
+vec3(201, 195, 54),
+vec3(34, 10, 204),
+vec3(87, 40, 209),
+vec3(64, 63, 42)
 );
+
 
 float iter(vec2 c) {
 	vec2 z = vec2(0, 0);
 	for (int i = 0; i < maxIter; i++) {
 		z = vec2(z.x * z.x - z.y * z.y + c.x, 2 * z.x * z.y + c.y);
 		if (z.x * z.x + z.y * z.y > 4) {
-			return i;
-			//z = vec2(z.x * z.x - z.y * z.y + c.x, 2 * z.x * z.y + c.y);
-			//z = vec2(z.x * z.x - z.y * z.y + c.x, 2 * z.x * z.y + c.y);
-			//float mod = sqrt(z.x * z.x + z.y * z.y);
-			//return i - (log(log(mod))) / ln2;
+			z = vec2(z.x * z.x - z.y * z.y + c.x, 2 * z.x * z.y + c.y);
+			z = vec2(z.x * z.x - z.y * z.y + c.x, 2 * z.x * z.y + c.y);
+			float mod = sqrt(z.x * z.x + z.y * z.y);
+			return i - log(log(mod)) / ln2;
 		}
 	}
 	return maxIter;
@@ -43,22 +46,29 @@ vec3 lerpClr(float n, vec3 a, vec3 b) {
 	return vec3(cr / 255, cg / 255, cb / 255);
 }
 
+float map(float n, float inputLow, float inputHigh, float outputLow, float outputHigh) {
+	return (n - inputLow) * (outputHigh - outputLow) / (inputHigh - inputLow) + outputLow;
+}
+
 vec3 calcClr(float i) {
-	int idx1 = int(i * colours.length());
-	int idx2 = idx1 + 1;
-	if (idx2 >= colours.length) idx2 = 0;
-	vec3 clr1 = colours[idx1];
-	vec3 clr2 = colours[idx2];
-	float ratio = mod(i, float(1) / colours.length()) / colours.length();
-	return lerpClr(ratio, clr1, clr2);
+	float j = 0;
+	int count = 0;
+	for (; j + float(1) / (colours.length() - 1) < i; j += float(1) / (colours.length() - 1), count++) {
+	}
+	i = map(i, float(count) / (colours.length() - 1), float(count + 1) / (colours.length() - 1), 0, 1);
+	return lerpClr(i, colours[count], colours[count + 1]);
 }
 
 void main(void) {
 	vec2 z = fpos;
+	
 	z.y /= aspectRatio;
+	z = (vec4(z, 0, 1) * rot).xy;
 	z.x *= zoom;
 	z.y *= zoom;
 	z += pos;
+
+	
 
 	float iter = iter(z);
 	if (iter == maxIter) frag = vec4(0, 0, 0, 1);
--- Mandelbrot OpenGL/Program.cs
+++ Mandelbrot OpenGL/Program.cs
@@ -9,7 +9,7 @@ namespace Mandelbrot {
     static class Program {
 
         static int width = 1200, height = 800;
-        const string titleName = "Mandelbrot Renderer";
+        const string titleName = "Mandelbrot Renderer - Copyright Alex Tan 2016";
 
         static ShaderProgram shader;
         static VBO<Vector2> vertices;
@@ -17,37 +17,42 @@ namespace Mandelbrot {
 
         static bool[] keys = new bool[255];
         static Vector2 pos = new Vector2(0, 0);
-        static float zoom = 1;
+        static float zoom = 2;
+        static float angle = 0;
+        static float maxIter = 1500f;
 
         static float prevTime;
         static float deltaTime;
+        static float countTime;
+        static int countTimeFrames;
+        static int fps;
         static Stopwatch watch = new Stopwatch();
 
 
         static void Main() {
             Glut.glutInit();
             Glut.glutInitDisplayMode(Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+
+            // width = Glut.glutGet(Glut.GLUT_SCREEN_WIDTH) == 0 ? width : Glut.glutGet(Glut.GLUT_SCREEN_WIDTH);
+            // height = Glut.glutGet(Glut.GLUT_SCREEN_HEIGHT) == 0 ? height : Glut.glutGet(Glut.GLUT_SCREEN_HEIGHT);
+            //Glut.glutGameModeString(width + "x" + height + ":32@60");
+
             Glut.glutInitWindowSize(width, height);
             Glut.glutCreateWindow(titleName);
-            //Glut.glutGameModeString(Width+"x"+Height+":32@60");
-
-            //width = Glut.glutGet(Glut.GLUT_SCREEN_WIDTH) == 0 ? width : Glut.glutGet(Glut.GLUT_SCREEN_WIDTH);
-           // height = Glut.glutGet(Glut.GLUT_SCREEN_HEIGHT) == 0 ? height : Glut.glutGet(Glut.GLUT_SCREEN_WIDTH);
-
 
             Glut.glutKeyboardFunc(OnKeyDown);
             Glut.glutKeyboardUpFunc(OnKeyUp);
-            Glut.glutMouseWheelFunc(OnMouseWheel);
 
             Gl.Viewport(0, 0, width, height);
+            //Glut.glutEnterGameMode();
             Glut.glutDisplayFunc(delegate () { });
             Glut.glutIdleFunc(MainGameLoop);
-
-            Console.WriteLine(Glut.glutExtensionSupported(" ARB_gpu_shader_fp64"));
 
             Init();
 
             Glut.glutMainLoop();
+
+            CleanUp();
 
         }
 
@@ -74,16 +79,45 @@ namespace Mandelbrot {
             float curTime = watch.ElapsedMilliseconds;
             deltaTime = curTime - prevTime;
             prevTime = curTime;
-            int fps = (int)(1000f / deltaTime);
-            if (fps < int.MaxValue && fps > int.MinValue) Glut.glutSetWindowTitle(titleName + " FPS: " + fps);
+            countTime += deltaTime;
+            countTimeFrames++;
+            if (countTime > 1000) {
+                fps = countTimeFrames;
+                countTime = 0;
+                countTimeFrames = 0;
+            }
+            Glut.glutSetWindowTitle(titleName + " | Zoom: " + String.Format("{0:0.000000}", zoom) + " | Max Iterations: " + (int)maxIter + " | FPS: " + fps);
             watch.Start();
 
-            float amt = deltaTime / 1000;
+            float amt = deltaTime / 2000;
+            if (keys['w']) {
+                pos.x += amt * zoom * (float)Math.Sin(angle);
+                pos.y += amt * zoom * (float)Math.Cos(angle);
+            }
+            if (keys['s']) {
+                pos.x -= amt * zoom * (float)Math.Sin(angle);
+                pos.y -= amt * zoom * (float)Math.Cos(angle);
+            }
+            if (keys['d']) {
+                pos.x += amt * zoom * (float)Math.Cos(angle);
+                pos.y -= amt * zoom * (float)Math.Sin(angle);
+            }
+            if (keys['a']) {
+                pos.x -= amt * zoom * (float)Math.Cos(angle);
+                pos.y += amt * zoom * (float)Math.Sin(angle);
+            }
 
-            if (keys['w']) pos.y += amt * zoom;
-            if (keys['s']) pos.y -= amt * zoom;
-            if (keys['d']) pos.x += amt * zoom;
-            if (keys['a']) pos.x -= amt * zoom;
+            if (keys['i']) zoom = zoom * (float)Math.Pow(0.9995, deltaTime);
+            if (keys['k']) zoom = zoom / (float)Math.Pow(0.9995, deltaTime);
+            if (zoom < 0.0001f) zoom = 0.0001f;
+            if (zoom > 5f) zoom = 5f;
+
+            if (keys['j']) angle -= deltaTime / 4000;
+            if (keys['l']) angle += deltaTime / 4000;
+
+            float iterFactor = 0.2f;
+            if (keys['y']) maxIter += deltaTime * iterFactor;
+            if (keys['h']) maxIter -= deltaTime * iterFactor;
 
             Render();
 
@@ -95,6 +129,8 @@ namespace Mandelbrot {
             shader["pos"].SetValue(pos);
             shader["aspectRatio"].SetValue((float)width / height);
             shader["zoom"].SetValue(zoom);
+            shader["rot"].SetValue(Matrix4.CreateRotationZ(angle));
+            shader["maxIter"].SetValue((int)maxIter);
             Gl.BindBufferToShaderAttribute(vertices, shader, "vpos");
             Gl.BindBuffer(elements);
             Gl.DrawElements(BeginMode.TriangleStrip, elements.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
@@ -102,16 +138,18 @@ namespace Mandelbrot {
 
         private static void OnKeyDown(byte key, int x, int y) {
             keys[key] = true;
+            if (key == 27) Glut.glutLeaveMainLoop();
         }
 
         private static void OnKeyUp(byte key, int x, int y) {
             keys[key] = false;
         }
 
-        private static void OnMouseWheel(int wheel, int direction, int x, int y) {
-            float amount = 0.95f;
-            if (direction < 0) zoom /= amount;
-            else zoom *= amount;
+        private static void CleanUp() {
+            vertices.Dispose();
+            elements.Dispose();
+            shader.DisposeChildren = true;
+            shader.Dispose();
         }
 
         private static string LoadFromFile(string source) {
